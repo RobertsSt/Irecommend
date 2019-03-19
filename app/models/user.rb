298201c9813ef8fec624acc9a1628893fc0b7d41
Roberts Steins <robertssteins@Admins-MacBook-Air.local>
2019-03-19 10:37:33 +0200

    Twitter 1.0 Responsivness fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,19 @@ class User < ApplicationRecord
     self.followings_as_follower.any? {|following| following.following_user_id == user.id}
   end
 
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+  
   def whom_to_follow(limit = 3)
     User.order("random()").where("users.id != (?)", id).joins(
       "LEFT OUTER JOIN followings ON followings.following_user_id = users.id AND followings.follower_user_id = #{id}"
