b2f441603eec0066e13f4142c19ebcd31664686a
Roberts Steins <robertssteins@Admins-MacBook-Air.local>
2019-03-19 17:22:10 +0200

    fixednavbar 3

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,11 @@ Rails.application.routes.draw do
     end
   end
 
-
+  delete 'session/signout', to: 'devise/sessions#destroy', as: :signout
   get ':username', to: 'users#show', as: :profile
   get ':username/edit', to: 'users#edit', as: :edit_profile
   post "users/:username", to: 'users#update', as: :update_profile
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweets#index"
